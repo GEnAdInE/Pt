a5606010bb7d2332af81fab90e7d816441d38a16
GEnAdInE <one.robin@outlook.fr>
2022-05-20 12:50:28 +0200

    Updating View model

---
+++ Task2/Presentation/Model/DataLayer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.DataLayer;
+
+namespace Presentation.Model
+{
+    public class DataLayer : AbstractDataAPI
+    {
+
+        private List<State>? localstates;
+        private List<User>? localusers;
+        private List<Catalog>? localCatalog;
+        private List<Event>? localEvent;
+
+        public override List<State> states { get { return localstates; } set { localstates = value; } }
+        public override List<User> users { get { return localusers; } set { localusers = value; } }
+        public override List<Catalog> catalogs { get { return localCatalog; } set { localCatalog = value; } }
+        public override List<Event> events { get { return localEvent; } set { localEvent = value; } }
+
+
+        public override State FindBook(string title, string author)
+        {
+            try
+            {
+
+                return states.Where(x => x.Book.Title == title && x.Book.Author == author).First();
+
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+        }
+        public override void Borrow(string Title, string Author, string Name, string Surname)
+        {
+            State state = FindBook(Title, Author);
+            if (state != null)
+            {
+                User User = FindUser(Name, Surname);
+                if (User != null)
+                {
+                    events.Add(new Borrowing(state, User));
+                    state.ChangeState();
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("User not found");
+                }
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Book not found");
+            }
+
+        }
+
+        public override void Return(string Title, string Author, string Name, string Surname)
+        {
+            User User = FindUser(Name, Surname);
+            if (User == null)
+            {
+                throw new Exception("User not found");
+            }
+            else
+            {
+                State state = FindBook(Title, Author);
+                if (state != null)
+                {
+                    events.Add(new Returning(state, User));
+                    state.ChangeState();
+
+                }
+                else
+                {
+                    throw new Exception("Book not found");
+                }
+            }
+        }
+
+        public override User FindUser(string name, string surname)
+        {
+            try
+            {
+                return users.Where(x => x.Name == name && x.Surname == surname).First();
+
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+        }
+        public override bool isAvailible(Catalog book)
+        {
+            if (book != null)
+            {
+                return FindBook(book.Title, book.Author).Available;
+            }
+            return false;
+        }
+        public override void addBook(Catalog catalog)
+        {
+            catalogs.Add(catalog);
+            states.Add(new State(catalog));
+        }
+        public override void addUser(User user)
+        {
+            users.Add(user);
+        }
+    }
+}
--- Task2/Presentation/ViewModel/MainWindowViewModel.cs
+++ Task2/Presentation/ViewModel/MainWindowViewModel.cs
@@ -4,11 +4,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task2.DataLayer;
+using System.Collections.ObjectModel;
+using Task2.Presentation.ViewModel.Base;
 
 namespace Task2.Presentation.ViewModel
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ViewModelBase
     {
-        MainWindowModel mainModel = new MainWindowModel();
+
+        private ObservableCollection<User> ?m_users;
+        public ObservableCollection<User> Users
+        {
+            get => m_users;
+            set
+            {
+                m_users = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Catalog> ?m_catalogs;
+        public ObservableCollection<Catalog> Catalogs
+        {
+            get => m_catalogs;
+            set
+            {
+                m_catalogs = value;
+                RaisePropertyChanged();
+            }
+        } 
+
+        private User ?m_user;
+        public User SelectedUser
+        {
+            get => m_user;
+            set
+            {
+                m_user = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private Catalog ?m_catalog;
+        public Catalog SelectedCatalog
+        {
+            get => m_catalog;
+            set
+            {
+                m_catalog = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand ?FetchDataCommend
+        {
+            get; private set;
+        }
+
+
+        public void MainWindowModel()
+        {
+            FetchDataCommend = new RelayCommand(() => { });
+
+        }
+
     }
+
+   
 }
